a09675247ab54743e134af54c7cac20900fa7bb9
Tiagoliveiras <tiago.25oliveir@gmail.com>
2025-03-03 08:41:09 -0300

    Atualização

--- index.php
+++ index.php
@@ -3,33 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stoke</title>
+    <title>Stoker</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <header>
-        <h1 class="titulo"><a href="index.php">Stoke</a></h1>
+        <h1><a class="link-titulo" href="index.php">Stoker</a></h1>
     </header>
     <main>
-        <section class="sec-login">
-            <div class="titulo-login">
-                <h2>Entrar</h2>
+        <section class="s-form">
+            <div class="box-titulo">
+                <h2>Login</h2>
             </div>
-            <div class="form-login">
-                <form action="index.php" method="post">
+            <div class="box-content">
+                <form action="login.php" name="form-login" class="form-login" method="post">
                     <fieldset>
-                        <label for="username">Usuário</label>
-                        <input type="text" name="username" id="username">
+                        <label for="user">Usuário</label>
+                        <input type="text" name="c-user" id="user" placeholder="Nome de usuário">
                     </fieldset>
                     <fieldset>
                         <label for="senha">Senha</label>
-                        <input type="password" name="senha" id="senha">
+                        <input type="password" name="c-senha" id="senha" placeholder="senha">
                     </fieldset>
                     <fieldset>
-                        <input type="submit" value="Entrar">
+                        <input type="checkbox" name="c-chk" id="chk">
+                        <label for="chk">Mantenha conectado</label>
+                        <a href="senha.php">Recuperar senha</a>
+                    </fieldset>
+                    <fieldset>
+                        <input type="submit" name="btn-entrar" id="entrar" value="Entrar">
                     </fieldset>
                 </form>
             </div>
         </section>
     </main>
+    <footer>
+        <p>&COPY;2024 - Todos os direitos reservados</p>
+    </footer>
 </body>
 </html>
